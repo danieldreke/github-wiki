ca9fcbb237027c31e3fd49b72e6448cb9f279aaa
Daniel Dreke <22680274+danieldreke@users.noreply.github.com>
2024-08-01 20:07:12 +0200

    Update fix_edit_url.py

--- fix_edit_url.py
+++ fix_edit_url.py
@@ -1,10 +1,7 @@
-from mkdocs.plugins import BasePlugin
-from mkdocs.config import config_options
+import os
 
-class WikiEditUrlPlugin(BasePlugin):
-    def on_page_context(self, context, page, config, nav):
-        if 'edit_url' in context:
-            edit_url = context['edit_url']
-            if edit_url.endswith('.md'):
-                context['edit_url'] = edit_url[:-3] + '/_edit'
-        return context
+def on_page_markdown(markdown, page, config, files):
+    if page.edit_url:
+        page_name = os.path.splitext(os.path.basename(page.file.src_path))[0]
+        page.edit_url = f"{config['repo_url']}/{page_name}/_edit"
+    return markdown
