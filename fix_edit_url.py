bb92c2890a2b69b0001a908474c79258ce0f2542
Daniel Dreke <22680274+danieldreke@users.noreply.github.com>
2024-08-02 03:33:51 +0200

    Update fix_edit_url.py

--- fix_edit_url.py
+++ fix_edit_url.py
@@ -3,7 +3,8 @@ import os
 def on_page_markdown(markdown, page, config, files):
     if page.edit_url:
         page_name = os.path.splitext(os.path.basename(page.file.src_path))[0]
-        if page_name == 'index':
-            page_name == 'Home'
-        # page.edit_url = f"{config['repo_url']}/{page_name}/_edit"
+        # if page_name == 'index':
+            # page_name == 'Home'
+        page.edit_url = f"{config['repo_url']}/{page_name}/_edit"
+        page.edit_url = page.edit_url.replace('/index/_edit', '/Home/_edit')
     return markdown
