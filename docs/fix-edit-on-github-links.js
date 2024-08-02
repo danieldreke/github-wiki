0e7d102df5db93a94d08a0788a5280f27b633016
Daniel Dreke <22680274+danieldreke@users.noreply.github.com>
2024-08-02 05:11:26 +0200

    Delete docs/fix-edit-on-github-links.js

--- docs/fix-edit-on-github-links.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const editLinks = document.querySelectorAll('a[href*="/_edit/"]');
-    editLinks.forEach(link => {
-        let href = link.getAttribute('href');
-        href = href.replace('/_edit/', '/');
-        href = href.replace('.md', '/_edit')
-        link.setAttribute('href', href);
-    });
-});
